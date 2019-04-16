d4c7efb33aa5d1ac4dfa823ab981ee47d04b0093
Ahbaid Gaffoor <ahbaid@gmail.com>
2019-04-15 22:24:34 -0700

    kcc

---
+++ lesson-003/turtle-1.py
@@ -0,0 +1,24 @@
+import turtle
+t=turtle.Pen()
+
+turtle.bgcolor('black')
+t.color('red','yellow')
+
+t.width(5)
+
+t.pencolor('yellow')
+t.forward(100)
+
+t.left(90)
+t.pencolor('red')
+t.forward(100)
+
+t.left(90)
+t.pencolor('green')
+t.forward(100)
+
+t.left(90)
+t.pencolor('blue')
+t.forward(100)
+
+x=input("Press enter to quit....")
